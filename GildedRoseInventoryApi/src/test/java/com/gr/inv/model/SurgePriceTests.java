a43a2d1bf6eff6affe1ce5d98d66dffeb3d99428
Tomas <Tomas@DESKTOP-39IA588.telus>
2017-04-13 18:14:45 -0700

    Tweaked surge test

--- GildedRoseInventoryApi/src/test/java/com/gr/inv/model/SurgePriceTests.java
+++ GildedRoseInventoryApi/src/test/java/com/gr/inv/model/SurgePriceTests.java
@@ -10,11 +10,22 @@ public class SurgePriceTests {
 	public void testSurge() {
 		
 		{
+			
+			/* 
+			 * The long list of simulated views makes sure the following scenarios verified.
+			 * a)The internal array (in this case size 3) gets wrapped.
+			 * b) Surge happens on the nth view. In this case the fourth. See long 1907
+			 * c) Surge happens on some view after the nth view. see long 1903
+			 * d)  Reset of class after surge happens. All test cases immediately after the multiplier goes up
+			 * e) The class maintains the surge price state for the duration of the application. So give it a good workout by 
+			 * supplying a bunch of numbers.
+			 * 
+			 */
 			SurgePricer pricer = new SurgePricerDeterministic(
 					100, // surgeTriggerWindowMilliseconds
-					4, //numberOfPreviousViews
+					4, // number of views in window to trigger surge
 					10, //surgePriceIncrementPercent 
-					1000, 1050, 1075, 1200, 1225, 1250, 1300, 1301, 1302, 1303, 1304, 1400, 1500, 1600, 1700, 1800, 1900, 1901, 1902, 1903//simulatedViewTimes
+					1000, 1050, 1075, 1200, 1225, 1250, 1300, 1301, 1302, 1303, 1304, 1400, 1500, 1600, 1700, 1800, 1900, 1901, 1902, 1903, 1904, 1905, 1906, 1907//simulatedViewTimes
 					);
 			
 			assertEquals(100,pricer.calculateSurgePriceMultiplierPercent()); // 1000
@@ -38,6 +49,11 @@ public class SurgePriceTests {
 			assertEquals(120,pricer.calculateSurgePriceMultiplierPercent()); // 1901
 			assertEquals(120,pricer.calculateSurgePriceMultiplierPercent()); // 1902
 			assertEquals(130,pricer.calculateSurgePriceMultiplierPercent()); // 1903
+			
+			assertEquals(130,pricer.calculateSurgePriceMultiplierPercent()); // 1904
+			assertEquals(130,pricer.calculateSurgePriceMultiplierPercent()); // 1905
+			assertEquals(130,pricer.calculateSurgePriceMultiplierPercent()); // 1906
+			assertEquals(140,pricer.calculateSurgePriceMultiplierPercent()); // 1907
 		}
 		
 		{
